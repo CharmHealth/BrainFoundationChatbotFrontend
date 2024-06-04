b5a2b2a61fbdeb3add0fda8ebae332812a217fe6
Rathi <kashi.rathi16@gmail.com>
2024-06-04 14:15:03 -0700

    new frontend, removed server

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
 import logo from './logo.svg';
-import NavBar from './navbar';
+import Navbar from './navbar';
+import Header from './header';
+import './App.css';
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [currentMessage, setCurrentMessage] = useState('');
-
-  const handleSendMessage = () => {
-    if (currentMessage.trim() !== '') {
-      const newMessage = {
-        text: currentMessage,
-        author: 'user',
-      };
-      setMessages([...messages, newMessage]);
-
-      // Send the current message to the Flask server
-      fetch(`http://127.0.0.1:5000/answerQuestion/?query=${encodeURIComponent(currentMessage)}`)
-        .then(response => response.json())
-        .then(data => {
-          // Assuming the server response contains the field 'response'
-          console.log("Data --> ", data)
-          const botResponse = {
-            text: data.response,
-            author: 'bot',
-          };
-          // Update the messages state to include the bot's response
-          setMessages(prevMessages => [...prevMessages, botResponse]);
-        })
-        .catch(error => {
-          console.error('Error during fetch:', error);
-          // Handle the error state appropriately in your UI
-        });
-
-      setCurrentMessage('');
-    }
-  };
-
-  const handleInputChange = (event) => {
-    setCurrentMessage(event.target.value);
-  };
-
-  const handleInputKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      handleSendMessage();
-    }
-  };
-
-
-
   return (
     <div className="App">
-          <nav className="nav-bar">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-      <div>
-        <h1>Ask Brain Foundation</h1>
-      </div>
+      <Navbar/>
       <header className="App-header">
-        <div className="chat-container">
-          <div className="messages">
-            {messages.map((message, index) => (
-              <div key={index} className={`message ${message.author === 'user' ? 'user' : 'bot'}`}>
-                <span>{message.text}</span>
-              </div>
-            ))}
-          </div>
-          <div className="input-area">
-            <input
-              type="text"
-              value={currentMessage}
-              onChange={handleInputChange}
-              onKeyDown={handleInputKeyDown}
-            />
-            <button onClick={handleSendMessage}>Send</button>
-          </div>
-        </div>
+        <Header/>
+        
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+
       </header>
     </div>
   );
